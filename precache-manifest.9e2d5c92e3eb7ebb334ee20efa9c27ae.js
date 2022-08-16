2f7eea5678354dde2b35ba8259e184da8aba40c1
trendsappdemo <amr.darawsheh+trenddemos@gmail.com>
2022-08-16 16:28:30 +0200

    publish

---
+++ precache-manifest.9e2d5c92e3eb7ebb334ee20efa9c27ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96b8838285a0f211f15b",
+    "url": "/trenddemo/static/css/main.c032bc7c.chunk.css"
+  },
+  {
+    "revision": "96b8838285a0f211f15b",
+    "url": "/trenddemo/static/js/main.c4a53832.chunk.js"
+  },
+  {
+    "revision": "7564e23ce0a6e5544783",
+    "url": "/trenddemo/static/js/runtime~main.70cb6c8b.js"
+  },
+  {
+    "revision": "7e6a43eeac186d5c68c0",
+    "url": "/trenddemo/static/js/2.1e6d9175.chunk.js"
+  },
+  {
+    "revision": "4b98ca23f7a2cd3866300336ac51e586",
+    "url": "/trenddemo/index.html"
+  }
+];
